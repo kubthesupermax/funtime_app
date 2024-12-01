5e9d9bb47af73f07906603ebec46cfc2b253d5c4
Franklin Kuba <franklinkuba7@gmail.com>
2024-12-01 13:40:56 -0600

    Navbar edit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">FunTime</Link>
+        <Link to="/">FunTime 😊</Link>
       </div>
       <div className="navbar-links">
         <Link to="/" className="nav-link">
